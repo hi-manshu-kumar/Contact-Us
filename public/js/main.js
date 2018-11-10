914bb78d241b51d62dc25c31e0ea5179c1e1857e
hi-manshu-kumar <himanshu.kumar394@gmail.com>
2018-11-10 12:08:52 +0530

    updated css

--- public/js/main.js
+++ public/js/main.js
@@ -26,3 +26,34 @@ function send(){
         console.log(err)
     });
 }
+
+// document.getElementsByClassName("clickChat").addEventListener("click", clickChat);
+var click = document.getElementsByClassName("clickChat");
+for(let i= 0; i<click.length; i++)
+    click[i].addEventListener('click', clickChat);
+
+function clickChat() {
+    document.getElementById("collect-chat-launcher-button").click();
+}
+
+var clickmail = document.getElementsByClassName("clickMail");
+for(let i= 0; i<clickmail.length; i++)
+    clickmail[i].addEventListener('click', clickMail);
+
+function clickMail(){
+    document.getElementById("mailContent").classList.add("show");
+    document.getElementById("mailContent").classList.remove("hide");
+    document.getElementById("content").classList.add("hide");
+    document.getElementById("mailContent").classList.remove("show");
+}
+
+var clicksms = document.getElementsByClassName("clickSMS");
+for(let i= 0; i<clicksms.length; i++)
+    clicksms[i].addEventListener('click', clickSMS);
+
+function clickSMS(){
+    document.getElementById("content").classList.add("show");
+    document.getElementById("content").classList.remove("hide");
+    document.getElementById("mailContent").classList.add("hide");
+    document.getElementById("mailContent").classList.remove("show");
+}
